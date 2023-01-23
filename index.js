8e861fd53834670b50ac26a71d157f6816c77d24
Suyash Mankar <suyashmankar@gmail.com>
2023-01-24 03:38:21 +0530

    setup folder structure

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+const bodyParser = require("body-parser");
+// const db = require("./config/mongoose");
+
+// use bodyparser
+// app.use(bodyParser.json());
+
+// use routes
+app.use("/", require("./routes"));
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log("error", err);
+  }
+  console.log("express server is up and running on port", port);
+});
